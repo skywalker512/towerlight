b6f55a76e1faa4bc4a6cb8a0ad6840d6df2ebc85
skywalker512 <houzhenhong@outlook.com>
2020-12-21 17:37:00 +0800

    feat(main): add next support

--- apps/main/src/app/app.controller.ts
+++ apps/main/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -6,8 +6,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Render('index')
   @Get()
-  getData() {
-    return this.appService.getData();
+  public index(@Query('name') name?: string) {
+    return { name };
   }
 }
--- apps/main/src/app/app.module.ts
+++ apps/main/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-
+import Next from 'next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NextAdapterModule } from '@towerlight/nest-next-adapter';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    NextAdapterModule.forRootAsync(
+      Next({
+        dev: process.env.NODE_ENV !== 'production',
+        dir: resolve(__dirname, '../../../apps/forum'),
+      }),
+      {
+        viewsDir: ''
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
