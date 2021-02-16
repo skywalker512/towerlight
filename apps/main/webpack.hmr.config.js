6d2f3cb707478966d41e93e44b49d1f857f72291
skywalker512 <houzhenhong@outlook.com>
2021-02-16 10:55:11 +0800

    fix: remove externals

--- apps/main/webpack.hmr.config.js
+++ apps/main/webpack.hmr.config.js
@@ -10,8 +10,7 @@ module.exports = (config) => {
     externals: [
       nodeExternals({
         allowlist: ['webpack/hot/poll?100']
-      }),
-      '@nestjs/platform-fastify'
+      })
     ],
     plugins: [
       ...config.plugins,
